1c01c372729d5fab929ed0f492c4033cb4d9429f
matteo102030 <124403162+matteo102030@users.noreply.github.com>
2023-03-17 16:49:02 -0500

    Add files via upload
    mateo falla muñoz

---
+++ pagina dinamica (corregido)/funcion/function.php
@@ -0,0 +1,18 @@
+<?php
+if (isset($_POST['submit'])){
+    $user=$_POST['user'];
+    $pass=$_POST['pass'];
+    $userDB="pedro@gmail.com";
+    $passDB="123";
+
+    if (isset($_POST['user'])){
+        if ($user=== $userDB){
+            header("location: ../home.php");
+        }
+        else{
+            echo'<script language="Javascript">alert("Erros en datos");</script>';
+        }
+    }
+}
+
+?>
